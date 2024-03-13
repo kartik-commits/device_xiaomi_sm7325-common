aeb0e58047fe3c6ea773c29adc327960674a136d
kartik-commits <kartiklashkare@gmail.com>
2024-03-13 22:18:40 +0530

    sm7325-common: Build Graphics Allocator HAL service and implement it *Fixes:- E vndksupport: Could not load /vendor/lib64/libmialgoengine.so from sphal namespace: dlopen failed: library "android.hardware.graphics.allocator@3.0.so" not found: needed by /vendor/lib64/libmialgoengine.so in namespace sphal.
    Co-authored-by: thepriyanshujangid <priyanshujangid@yahoo.com>
    Co-authored-by: Choirul Umam <bavvtm@outlook.com>
    Signed-off-by: kartik-commits <kartiklashkare@gmail.com>

--- common.mk
+++ common.mk
@@ -155,6 +155,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@2.0.vendor \
 
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@3.0 \
+    android.hardware.graphics.allocator@3.0-impl \
+    android.hardware.graphics.allocator@3.0-service \
     android.hardware.graphics.allocator@3.0.vendor \
     vendor.display.config@1.15.vendor \
     vendor.display.config@2.0.vendor
