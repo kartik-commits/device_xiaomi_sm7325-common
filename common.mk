d35da0c01342a820a71488db9f8dd76ad11c23cb
Alexandru Scurtu <sasha.scurtu@gmail.com>
2024-04-21 17:18:59 +0200

    sm7325-common: Build sensorservice 1.0 HAL
    *Fixes:-
    E init    : Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 1041 (/system/bin/hwservicemanager)
    
    Signed-Off-By: kartik-commits	<kartiklashkare@gmail.com>

--- common.mk
+++ common.mk
@@ -438,6 +438,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors-service.xiaomi-multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
