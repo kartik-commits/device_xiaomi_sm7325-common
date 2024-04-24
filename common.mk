28260c5b905a3df01dd2644e246164debb9a4614
tweenk <tweenk.pl@gmail.com>
2024-04-24 13:31:05 +0530

    sm7325-common: Build prebuilt protobuf 3.9.1. Needed to work around a Qualcomm HAL prebuilt.
    *Fixes:-
      04-24 13:21:51.858  9186  9186 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrilNrd": library "libprotobuf-cpp-full-3.9.1.so" not found: needed by /vendor/lib64/libqcrilNr.so in namespace (default)
      04-24 13:21:51.861  9187  9187 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrilNrd": library "libprotobuf-cpp-full-3.9.1.so" not found: needed by /vendor/lib64/libqcrilNr.so in namespace (default)
    
    Bug: 203713560
    Bug: 258322017
    Bug: 258589373
    Bug: 258632688
    Test: Forrest
    Change-Id: I7c45f92295161537944175dc006ef4fc03d805ba
    Signed-off-by: kartik-commits <kartiklashkare@gmail.com>

--- common.mk
+++ common.mk
@@ -416,6 +416,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
     $(LOCAL_PATH)/configs/perf/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+    
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
