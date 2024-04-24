62583a6423581311c1b05498227cb9f5cdcd9ad4
aoleary <seanm187@gmail.com>
2024-04-24 13:43:21 +0530

    sm7325-common: Build android.hardware.sensors@2.1 *Fixes:-   04-24 13:19:19.096     1     1 E init    : Control message: Could not find 'android.hardware.sensors@2.1::ISensors/default' for ctl.interface_start from pid: 537 (/system/system_ext/bin/hwservicemanager) Signed-off-by: kartik-commits <kartiklashkare@gmail.com>

--- common.mk
+++ common.mk
@@ -450,6 +450,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.1 \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
