46566864516097fa2ecac4a9afd8292c97a131db
kartik-commits <kartiklashkare@gmail.com>
2024-08-24 18:22:18 +0530

    Revert "sm7325-common: Uprev audio HAL to 7.0"
    This reverts commit b5b6ccd15fa62f57875e71b7279214c820cb6171.

--- common.mk
+++ common.mk
@@ -48,8 +48,8 @@ PRODUCT_PACKAGES += \
     android.media.audio.common.types-V1-cpp
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl \
-    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl
 
