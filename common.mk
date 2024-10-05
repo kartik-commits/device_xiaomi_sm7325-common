eb2c0938a94b78bda96489cb15a70360c81f9298
kartik-commits <kartiklashkare@gmail.com>
2024-10-05 13:11:58 +0530

    sm7325-common: Purge android.hardware.sensors@2.1-service.xiaomi-multihal *Fixes:- Offending entries: android.hardware.sensors@2.1-service.xiaomi-multihal
    Signed-Off-By: kartik-commits	<kartiklashkare@gmail.com>

--- common.mk
+++ common.mk
@@ -463,7 +463,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1 \
-    android.hardware.sensors@2.1-service.xiaomi-multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
 PRODUCT_COPY_FILES += \
